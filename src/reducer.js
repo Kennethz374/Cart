2b7ebc77fd9a49ae24b843e586f0466c9b155503
kennethzhang <kennethzhang374@gmail.com>
2021-02-24 01:36:34 -0800

    write some comments

--- src/reducer.js
+++ src/reducer.js
@@ -7,27 +7,18 @@ export const reducer = (state, action) => {
 		return { ...state, cart: newCart };
 	}
 
-	// if (action.type === "TOGGLE_AMOUNT") {
-	// 	let tempCart = state.cart.map((cartItem) => {
-	// 		if (cartItem.id === action.payload.id) {
-	// 			if (action.payload.type === "inc") {
-	// 				return { ...cartItem, amount: cartItem.amount + 1 };
-	// 			}
-	// 		}
-	// 	});
-	// }
 	if (action.type === "TOGGLE_AMOUNT") {
 		let tempCart = state.cart
 			.map((cartItem) => {
 				if (cartItem.id === action.payload.id) {
 					if (action.payload.type === "inc") {
-						return { ...cartItem, amount: cartItem.amount + 1 };
+						return { ...cartItem, amount: cartItem.amount + 1 }; //meet condition modifie item
 					}
 					if (action.payload.type === "dec") {
-						return { ...cartItem, amount: cartItem.amount - 1 };
+						return { ...cartItem, amount: cartItem.amount - 1 }; //meet condition modifie item
 					}
 				}
-				return cartItem; //return the modified cartItem
+				return cartItem; //if didn't meet condition return items
 			})
 			.filter((cartItem) => cartItem.amount !== 0);
 		return { ...state, cart: tempCart };
